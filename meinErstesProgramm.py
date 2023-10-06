479983bb6d15bb7fe346a7e0a2f14df8ecb879d4
JenniferSchoch88 <96466599+JenniferSchoch88@users.noreply.github.com>
2023-10-06 11:26:37 +0200

    HelloWorld and meinErstesProgramm

---
+++ meinErstesProgramm.py
@@ -0,0 +1,11 @@
+"""meinErstesProgramm.py: Mein erstes Python Programm.
+Example:
+    python meinErstesProgramm.py
+"""
+__author__      = "Jennifer Schoch"
+__credits__     = ["Nelli, Fabio. Python data analytics with Pandas, NumPy, and Matplotlib. (2018)."]
+__email__       = "jennifer.schoch@dhbw-karlsruhe.de"
+
+
+meinname = input("Wie lautet Dein Name?")
+print("Hallo " + meinname + ", es freut mich zu sagen: Hallo Welt!")
