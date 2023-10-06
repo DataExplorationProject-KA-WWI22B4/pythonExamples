60180807623575da4e6225887aba0bbb80edffde
jeschoch <96466599+JenniferSchoch88@users.noreply.github.com>
2023-10-06 11:42:59 +0200

    Create calculations.py

---
+++ calculations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""calculations.py: Berechnungen.
+Example:
+    python calculations.py
+"""
+__author__      = "Jennifer Schoch"
+__credits__     = ["Nelli, Fabio. Python data analytics with Pandas, NumPy, and Matplotlib. (2018)."]
+__email__       = "jennifer.schoch@dhbw-karlsruhe.de"
+
+print("1+2=", 1+2)
+print("(1.045*3)/4=",(1.045*3)/4)
+print("4**2", 4**2)
+print("(4+5j)*(2+3j)", (4+5j)*(2+3j))
+print("4<(2*3)", 4<(2*3))
