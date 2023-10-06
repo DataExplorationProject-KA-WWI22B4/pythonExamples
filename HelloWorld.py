8b2ea3d4b0cb40dc6c91cdcab4941a019311c2ec
JenniferSchoch88 <96466599+JenniferSchoch88@users.noreply.github.com>
2023-10-06 11:41:08 +0200

    Adjustments

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,12 @@
-# Hallo Python!print("Hello World!")
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""helloWorld.py: Hallo Python.
+Example:
+    python helloWorld.py
+"""
+__author__      = "Jennifer Schoch"
+__credits__     = ["Nelli, Fabio. Python data analytics with Pandas, NumPy, and Matplotlib. (2018)."]
+__email__       = "jennifer.schoch@dhbw-karlsruhe.de"
+
+
+print("Hello World!")
--- meinErstesProgramm.py
+++ meinErstesProgramm.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """meinErstesProgramm.py: Mein erstes Python Programm.
 Example:
     python meinErstesProgramm.py
